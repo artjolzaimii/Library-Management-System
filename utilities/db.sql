02ac38eada927ae3bed432c14ec6731df2bc4531
Arjan Muka <muka.arjan@outlook.com>
2025-05-25 18:25:10 +0200

    New database, included borrwoed_books

--- utilities/db.sql
+++ utilities/db.sql
@@ -1,16 +1,78 @@
-CREATE TABLE genres (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL UNIQUE
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 25, 2025 at 06:23 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id VARCHAR(50) UNIQUE,
-    full_name VARCHAR(100),
-    email VARCHAR(100),
-    phone VARCHAR(20),
-    address TEXT,
-    username VARCHAR(50),
-    password TEXT,
-    role VARCHAR(20)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bookstore_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `borrow_book`
+--
+
+CREATE TABLE `borrow_book` (
+  `borrow_book_id` int(11) NOT NULL,
+  `book_id` int(11) DEFAULT NULL,
+  `inventory` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `borrow_book`
+--
+
+INSERT INTO `borrow_book` (`borrow_book_id`, `book_id`, `inventory`) VALUES
+(1, 1, 0),
+(2, 2, 13);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `borrow_book`
+--
+ALTER TABLE `borrow_book`
+  ADD PRIMARY KEY (`borrow_book_id`),
+  ADD KEY `book_id` (`book_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `borrow_book`
+--
+ALTER TABLE `borrow_book`
+  MODIFY `borrow_book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `borrow_book`
+--
+ALTER TABLE `borrow_book`
+  ADD CONSTRAINT `borrow_book_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `book` (`book_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
